6ba88bf6390c3eb4d7ed188db55df52274dd1216
David Gonzalez <dgonzalez@chapman.edu>
2025-11-16 17:03:37 -0800

    organized main.go for week1-basics

--- week1-basics/main.go
+++ week1-basics/main.go
@@ -1,23 +1,7 @@
 package main
 
-import "fmt"
-
 func main() {
-	backendJourney()
+	//backendJourney()
 	// guessingGame()
-}
-
-func backendJourney() {
-	fmt.Println("Starting my backend journey!")
-
-	name := "David" // := is type inference (like auto in C++)
-	age := 22
-
-	fmt.Printf("I'm %s, %d years old\n", name, age)
-
-	// Slices (like vectors in C++)
-	skills := []string{"C++", "Java", "Learning Go"}
-	for i, skill := range skills {
-		fmt.Printf("%d. %s\n", i+1, skill)
-	}
+	calculator()
 }
